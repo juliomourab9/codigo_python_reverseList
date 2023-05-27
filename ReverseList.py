54e23e5724b77ff0f1789228d982b3f52c2c5841
Julio Cesar Moura <97678727+juliomourab9@users.noreply.github.com>
2023-05-27 15:23:17 -0300

    Add files via upload

---
+++ ReverseList.py
@@ -0,0 +1,7 @@
+nome = input('Informe seu nome: ')
+
+lista = [] for i in nome: lista.append(i) print(lista)
+
+lista.reverse() print(lista)
+
+nome_inv = '' for i in lista: nome_inv = nome_inv + i #print(nome_inv) print(nome_inv)
